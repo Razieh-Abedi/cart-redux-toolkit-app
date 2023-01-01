8704ba37aff1ee9f2b5f51cdf937c7cad4521ea2
Razieh Abedi <raziyeabedi@gmail.com>
2023-01-01 16:20:34 +0800

    set calculate total functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { calculateTotal } from "./features/cart/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
